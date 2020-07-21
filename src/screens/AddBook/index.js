e5fa36e2c7faa8bfa5cb97abaeef2b38224def2d
Kauã Steliano <stelianok@gmail.com>
2020-07-21 11:20:14 -0300

    Finished Add Book Screen

--- src/screens/AddBook/index.js
+++ src/screens/AddBook/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+iniimport React, {useState} from 'react';
+import {
+  Text,
+  View,
+  TextInput,
+  useWindowDimensions,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from 'react-native';
 
 import styles from './styles';
 
 export default function AddBook() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const width = useWindowDimensions().width;
   return (
     <View style={styles.container}>
-      <Text>AddBook</Text>
+      <KeyboardAvoidingView style={styles.formView}>
+        <Text style={styles.title}>Add a new book</Text>
+
+        <TextInput
+          style={[styles.textInput, {width: width - 55}]}
+          onChangeText={(title) => setTitle(title)}
+          value={title}
+          placeholder={'Title - E.g: Harry Potter'}
+          placeholderTextColor={'gray'}
+          underlineColorAndroid={'black'}
+        />
+
+        <TextInput
+          style={[styles.textInput, {width: width - 55}]}
+          onChangeText={(author) => setAuthor(author)}
+          value={author}
+          placeholder={'Author - E.g: J.K Rowling'}
+          placeholderTextColor={'gray'}
+          underlineColorAndroid={'black'}
+        />
+
+        <TouchableOpacity
+          style={[styles.button, {width: width - 55}]}
+          onPress={() => {}}>
+          <Text style={styles.buttonText}> Add the new book </Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
--- src/screens/AddBook/styles.js
+++ src/screens/AddBook/styles.js
@@ -4,6 +4,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+
+    justifyContent: 'center',
+    alignItems: 'center',
   },
+  title: {
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 50,
+  },
+  formView: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    alignSelf: 'flex-start',
+    marginLeft: 20,
+    marginTop: 20,
+  },
+  textInput: {
+    marginTop: 50,
+    fontSize: 24,
+  },
+  button: {
+    backgroundColor: '#3f51b5',
+    borderRadius: 100,
+
+    marginTop: 25,
+
+    paddingVertical: 15,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24,
+    alignSelf: 'center',
+
+    
+  }
 });
 export default styles;
